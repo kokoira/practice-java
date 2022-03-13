104ff7651deae668c92253928eadedf79edbfb30
kokoira <95961416+kokoira@users.noreply.github.com>
2022-03-13 09:49:48 +0900

    Test

---
+++ src/test/helloworld.java
@@ -0,0 +1,12 @@
+package test;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      for (int i = 0; i < 10; i++) {
+	    System.out.println("Hello world");
+      }
+
+	}
+}
