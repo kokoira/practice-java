046b58958091170c5f24957142688144e8360498
kokoira <95961416+kokoira@users.noreply.github.com>
2022-03-14 20:38:33 +0900

    変数を宣言して利用してみる

--- src/test/Helloworld.java
+++ src/test/Helloworld.java
@@ -4,7 +4,13 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-	    System.out.println("Hello world");
+		char animal;
+		animal = '犬';
+		String name;
+		name = "ハチ";
+		int age;
+		age = 11;
+	    System.out.println(animal + "の" + name + "の年齢は、" + age + "歳です。");
 
 	}
 }
